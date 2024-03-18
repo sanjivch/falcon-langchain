1f155c65ec4f9fbdb28d00ea79cb168cd2bf9820
Sanjiv Chemudupati <sanjivch@users.noreply.github.com>
2024-03-18 19:03:54 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -2,25 +2,49 @@ import chainlit as cl
 
 import os
 huggingfacehub_api_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import HuggingFaceEndpoint
 
-from langchain import HuggingFaceHub, PromptTemplate, LLMChain
 
-repo_id = "tiiuae/falcon-7b-instruct"
-llm = HuggingFaceHub(huggingfacehub_api_token=huggingfacehub_api_token, 
-                     repo_id=repo_id, 
-                     model_kwargs={"temperature":0.6, "max_new_tokens":2000})
 
+repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id, max_length=128, temperature=0.5, huggingfacehub_api_token=huggingfacehub_api_token
+)
 template = """
-You are an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
+You are an helpful assistant expert at machine learning. Give helpful, detailed, and correct answers to the user's questions. Do not make up answers.
 
 {question}
 
 """
 
 
-@cl.langchain_factory
-def factory():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
-    return llm_chain
+@cl.on_chat_start
+def main():
+    # Instantiate the chain for that user session
+    print(template)
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    print(prompt)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    print(llm_chain)
+    # Store the chain in the user session
+    cl.user_session.set("llm_chain", llm_chain)
+
+
+@cl.on_message
+async def main(message: str):
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+    print(llm_chain)
+    # Call the chain asynchronously
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    # res=await chain.acall(message.content, callbacks=[cb])
+    # Do any post processing here
+    content = res['text']
+    content = content[content.find('Assistant:'):]
+
+    # Send the response
+    await cl.Message(content=content).send()
